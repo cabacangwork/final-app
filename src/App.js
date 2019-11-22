913c56f2b6555dde026e6008526643bcf6943263
Kass <cabacangwork@gmail.com>
2019-11-22 10:57:58 +0800

    Updates

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import React, {Component} from 'react';
 import RecipeContextProvider from './contexts/RecipeContext';
 import Navigation from './components/Navigation';
+import Home from './components/Home';
+import AddRecipe from './components/AddRecipe';
+import RecipeList from './components/RecipeList';
+import RecipeDetails from './components/RecipeDetails';
+import PageNotFound from './components/PageNotFound';
+import EditRecipe from './components/EditRecipe';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 
 class App extends Component{
   render() { 
     return (
         <RecipeContextProvider>
-            <Navigation/>
+            <Router>
+              <Navigation/>
+              <Switch>
+                  <Route exact path="/"><Home/></Route>
+                  <Route exact path="/addrecipe" component={AddRecipe}></Route>
+                  <Route exact path="/recipes" component={RecipeList}></Route>
+                  <Route exact path="/recipe/:id" component={RecipeDetails}/>
+                  <Route exact path="/recipe/update/:id" component={EditRecipe}/>
+                  <Route  component={PageNotFound} />
+              </Switch>
+            </Router>
         </RecipeContextProvider>
     )
   }
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,40 +1,19 @@
 import React, {Component} from 'react';
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    NavLink
-  } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import logo from '../../public/styles/images/logo-white.png';
-import Home from './Home';
-import AddRecipe from './AddRecipe';
-import RecipeList from './RecipeList';
-import RecipeDetails from './RecipeDetails';
-import PageNotFound from './PageNotFound';
-import EditRecipe from './EditRecipe';
 
 const Navigation = () => {
 
     return (
-        <Router>
-            <nav className="navbar">
-                <NavLink className="navbar-brand" exact to="/">
-                    <img src={logo} width="43" height="45" />
-                </NavLink>
-                <div className="form-inline">
-                    <NavLink className="nav-link" to="/addrecipe">Add Recipe</NavLink>
-                    <NavLink className="nav-link" to="/recipes">Recipe List</NavLink>
-                </div>
-            </nav>
-            <Switch>
-                <Route exact path="/"><Home/></Route>
-                <Route exact path="/addrecipe" component={AddRecipe}></Route>
-                <Route exact path="/recipes" component={RecipeList}></Route>
-                <Route exact path="/recipe/:id" component={RecipeDetails}/>
-                <Route exact path="/recipe/update/:id" component={EditRecipe}/>
-                <Route  component={PageNotFound} />
-            </Switch>
-        </Router>
+        <nav className="navbar">
+            <NavLink className="navbar-brand" exact to="/">
+                <img src={logo} width="43" height="45" />
+            </NavLink>
+            <div className="form-inline">
+                <NavLink className="nav-link" to="/addrecipe">Add Recipe</NavLink>
+                <NavLink className="nav-link" to="/recipes">Recipe List</NavLink>
+            </div>
+        </nav>
     )
 
 }

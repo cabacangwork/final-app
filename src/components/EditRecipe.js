c926628cab6aab621d5c5c939524f1dff7bce8c8
Kass <cabacangwork@gmail.com>
2019-12-04 17:10:10 +0800

    update

--- src/components/EditRecipe.js
+++ src/components/EditRecipe.js
@@ -14,7 +14,8 @@ class EditRecipe extends Component{
         ingredients: [''],
         procedures: [''],
         dish: 'not-specified',
-        notFound: false
+        notFound: false,
+        loading: true
     }
 
     componentDidMount() {
@@ -42,69 +43,71 @@ class EditRecipe extends Component{
 
         const { match } = this.props;
         const route_id = match.params.id;
-        const { title, description, dish, ingredients, procedures, notFound } = this.state
+        const { title, description, dish, ingredients, procedures, notFound, loading } = this.state
 
         return (
             <div>
-                {notFound?  <PageNotFound/> :
-                    <div className="form-add card container edit">
-                        <Link to={`/recipes/view/${route_id}`} className="back-link">&lt; Back to Recipe Details</Link>
-                        <h2 className="card-title">Edit Recipe</h2>
-                        <div className="edit-form">
-                            <form onSubmit={this.handleSubmit}>
-                                <div className="form-group">
-                                    <label>Title</label><br/>
-                                    <input className="form-control" type="text" name="title" value={title} onChange={this.handleChange} required />
-                                </div>
-                                <div className="form-group">
-                                    <label>Description</label><br/>
-                                    <textarea className="form-control" rows="3" type="text" name="description" value={description} onChange={this.handleChange} required />
-                                </div>
-                                <div className="form-group">
-                                    <label>Dish Type</label>
-                                    <select className="form-control" id="dishType" value={dish} onChange={(e) => this.onOption(e)}>
-                                        <option value="not-specified">Not Specified</option>
-                                        <option value="chicken">Chicken</option>
-                                        <option value="beef">Beef</option>
-                                        <option value="pork">Pork</option>
-                                        <option value="seafood">Seafood</option>
-                                        <option value="vegetable">Vegetable</option>
-                                        <option value="pasta">Pasta</option>
-                                        <option value="desert">Desert</option>
-                                    </select>
-                                </div>
-                                <div className="form-group">
-                                    <label>Ingredients:</label>
-                                    { ingredients && ingredients.map((ing, index) => (
-                                        <div key={index} className="add-tab">
-                                            <div className="add-input">
-                                                <input className="form-control" value={ing} onChange={(e) => this.onChangeIngredient(e, index)} required/>
+                {loading? <span className="loading">Loading...</span> : 
+                    (notFound?  <PageNotFound/> :
+                        <div className="form-add card container edit">
+                            <Link to={`/recipes/view/${route_id}`} className="back-link">&lt; Back to Recipe Details</Link>
+                            <h2 className="card-title">Edit Recipe</h2>
+                            <div className="edit-form">
+                                <form onSubmit={this.handleSubmit}>
+                                    <div className="form-group">
+                                        <label>Title</label><br/>
+                                        <input className="form-control" type="text" name="title" value={title} onChange={this.handleChange} required />
+                                    </div>
+                                    <div className="form-group">
+                                        <label>Description</label><br/>
+                                        <textarea className="form-control" rows="3" type="text" name="description" value={description} onChange={this.handleChange} required />
+                                    </div>
+                                    <div className="form-group">
+                                        <label>Dish Type</label>
+                                        <select className="form-control" id="dishType" value={dish} onChange={(e) => this.onOption(e)}>
+                                            <option value="not-specified">Not Specified</option>
+                                            <option value="chicken">Chicken</option>
+                                            <option value="beef">Beef</option>
+                                            <option value="pork">Pork</option>
+                                            <option value="seafood">Seafood</option>
+                                            <option value="vegetable">Vegetable</option>
+                                            <option value="pasta">Pasta</option>
+                                            <option value="desert">Desert</option>
+                                        </select>
+                                    </div>
+                                    <div className="form-group">
+                                        <label>Ingredients:</label>
+                                        { ingredients && ingredients.map((ing, index) => (
+                                            <div key={index} className="add-tab">
+                                                <div className="add-input">
+                                                    <input className="form-control" value={ing} onChange={(e) => this.onChangeIngredient(e, index)} required/>
+                                                </div>
+                                                <div>
+                                                    <button className="btn btn-remove" onClick={(e) => this.removeIngredient(e, index)}>Remove</button>
+                                                </div>
                                             </div>
-                                            <div>
-                                                <button className="btn btn-remove" onClick={(e) => this.removeIngredient(e, index)}>Remove</button>
+                                        ))}
+                                        <br/><button onClick={this.addIngredient} className="btn btn-secondary btn-add">Add Ingredient</button>
+                                    </div>
+                                    <div className="form-group">
+                                        <label>Procedure:</label>
+                                        { procedures && procedures.map((procedure, index) => (
+                                            <div key={index} className="add-tab">
+                                                <div className="add-input">
+                                                    <textarea className="form-control" rows="3" type="text" value={procedure} onChange={(e) => this.onChangeProcedure(e, index)} required/>
+                                                </div>
+                                                <div>
+                                                    <button className="btn btn-remove" onClick={(e) => this.removeProcedure(e, index)}>Remove</button>
+                                                </div>
                                             </div>
-                                        </div>
-                                    ))}
-                                    <br/><button onClick={this.addIngredient} className="btn btn-secondary btn-add">Add Ingredient</button>
-                                </div>
-                                <div className="form-group">
-                                    <label>Procedure:</label>
-                                    { procedures && procedures.map((procedure, index) => (
-                                        <div key={index} className="add-tab">
-                                            <div className="add-input">
-                                                <textarea className="form-control" rows="3" type="text" value={procedure} onChange={(e) => this.onChangeProcedure(e, index)} required/>
-                                            </div>
-                                            <div>
-                                                <button className="btn btn-remove" onClick={(e) => this.removeProcedure(e, index)}>Remove</button>
-                                            </div>
-                                        </div>
-                                    ))}
-                                    <br/><button onClick={this.addProcedure} className="btn btn-secondary btn-add">Add Procedure</button>
-                                </div>   
-                                <button type="submit" className="btn btn-primary btn-lg">Submit</button>   
-                            </form>
+                                        ))}
+                                        <br/><button onClick={this.addProcedure} className="btn btn-secondary btn-add">Add Procedure</button>
+                                    </div>   
+                                    <button type="submit" className="btn btn-primary btn-lg">Submit</button>   
+                                </form>
+                            </div>
                         </div>
-                    </div>
+                    )
                 }
             </div>
         )
--- src/components/RecipeDetails.js
+++ src/components/RecipeDetails.js
@@ -31,48 +31,49 @@ class RecipeDetails extends Component {
         return (
             <div> 
                 <div className="recipe container">
-                {loading? 'loading...' : 
-                    (notFound? <PageNotFound/>:
-                    <div key={recipeInfo._id} className="details">
-                        <h2>{recipeInfo.title}</h2>
-                        <p>{recipeInfo.description}</p>
-                        <h5>Dish Type:</h5>
-                        <p className="dish-type">{recipeInfo.dish}</p>
-                        <div className="ingredients-info">
-                            <h5>Ingredients:</h5>
-                            <ul>
-                                { recipeInfo.ingredients.map((ing, index) => (
-                                    <li key={index}>{ing}</li>
-                                ))}
-                            </ul>
-                        </div>
-                        <div className="procedures-info">
-                            <h5>Procedures:</h5>
-                            <ol>
-                                { recipeInfo.procedures.map((pro, index) => (
-                                    <li key={index}>{pro}</li>
-                                ))}
-                            </ol>
-                        </div>
-                        <hr/>
-                        <div className="dates">
-                            <footer className="blockquote-footer">
-                                <cite title="Source Title">Recipe Added on: {recipeInfo.dateCreated}</cite>
-                            </footer>
-                            <footer className="blockquote-footer">
-                                <cite title="Source Title">Last Update on: {recipeInfo.editDate}</cite>
-                            </footer>
-                        </div>
-                        <div className="footer-buttons">
-                            <div className="left"><Link to="/recipes/list">&lt; Recipe List</Link></div>
-                            <div className="middle">
-                                <Link className="btn btn-info edit" to={`/recipes/update/${recipeInfo._id}`}>Edit</Link>
-                                <button type="button" className="btn btn-info" onClick={() => this.onDelete(recipeInfo._id)}>Delete</button>
+                    {loading? <span className="loading">Loading...</span> : 
+                        (notFound? <PageNotFound/>:
+                            <div key={recipeInfo._id} className="details">
+                                <h2>{recipeInfo.title}</h2>
+                                <p>{recipeInfo.description}</p>
+                                <h5>Dish Type:</h5>
+                                <p className="dish-type">{recipeInfo.dish}</p>
+                                <div className="ingredients-info">
+                                    <h5>Ingredients:</h5>
+                                    <ul>
+                                        { recipeInfo.ingredients.map((ing, index) => (
+                                            <li key={index}>{ing}</li>
+                                        ))}
+                                    </ul>
+                                </div>
+                                <div className="procedures-info">
+                                    <h5>Procedures:</h5>
+                                    <ol>
+                                        { recipeInfo.procedures.map((pro, index) => (
+                                            <li key={index}>{pro}</li>
+                                        ))}
+                                    </ol>
+                                </div>
+                                <hr/>
+                                <div className="dates">
+                                    <footer className="blockquote-footer">
+                                        <cite title="Source Title">Recipe Added on: {recipeInfo.dateCreated}</cite>
+                                    </footer>
+                                    <footer className="blockquote-footer">
+                                        <cite title="Source Title">Last Update on: {recipeInfo.editDate}</cite>
+                                    </footer>
+                                </div>
+                                <div className="footer-buttons">
+                                    <div className="left"><Link to="/recipes/list">&lt; Recipe List</Link></div>
+                                    <div className="middle">
+                                        <Link className="btn btn-info edit" to={`/recipes/update/${recipeInfo._id}`}>Edit</Link>
+                                        <button type="button" className="btn btn-info" onClick={() => this.onDelete(recipeInfo._id)}>Delete</button>
+                                    </div>
+                                    <div className="right"><Link to="/recipes/add">Add Recipe &gt;	</Link></div>                
+                                </div>
                             </div>
-                            <div className="right"><Link to="/recipes/add">Add Recipe &gt;	</Link></div>                
-                        </div>
-                    </div>)
-                }
+                        )
+                    }
                 </div>
             </div>
         )
